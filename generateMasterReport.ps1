b1f16329f0f661d9add4fd64c1a719c2d3841f5e
Jimmy <jpsietsma@users.noreply.github.com>
2018-05-24 20:55:56 -0400

    Create generateMasterReport.ps1

---
+++ generateMasterReport.ps1
@@ -0,0 +1,64 @@
+#Install-Module -Name ReportHTML
+
+$config_TVShowDrives = @(
+                            "E:\TV Shows",
+                            "G:\TV Shows",
+                            "H:\TV Shows",
+                            "I:\TV Shows"
+                        )
+
+$existingShows = Get-ChildItem $config_TVShowDrives -Directory | SELECT FullName, Name, Size | SORT name
+
+$Rpt = @()
+$Rpt += get-htmlopenpage -TitleText "Master TV Shows" 
+$Rpt += Get-HTMLContentOpen
+
+$Rpt += get-htmlcontentdatatable -ArrayOfObjects ($existingShows | SELECT Name, FullName, @{ expression={
+
+                                                                                                            $name = $_.FullName
+                                                                                                            $name = $name -split('\')
+
+                                                                                                            switch($name){
+
+                                                                                                                "E:" { $airStatus = 'Active' }
+                                                                                                                "G:" { $airStatus = 'Ended' }
+                                                                                                                "H:" { $airStatus = 'Active' }
+                                                                                                                "I:" { $airStatus = 'Ended' }
+
+                                                                                                                default { $airStatus = 'unknown' }
+
+                                                                                                            }
+
+                                                                                                        $airStatus
+
+                                                                                                        }; 
+                                                                                             
+                                                                                             label='Airing Status' }, 
+                                                                                             
+                                                                                             @{ expression={
+                                                                                             
+                                                                                                                (Get-ChildItem -Directory ($_.FullName) ).count
+                                                                                                                
+                                                                                                           }; 
+                                                                                                
+                                                                                                label='# of Seasons' }, 
+                                                                                                
+                                                                                             @{ expression={
+                                                                                             
+                                                                                                                (Get-ChildItem -Recurse -File ($_.FullName) ).count
+                                                                                                                
+                                                                                                           }; 
+                                                                                                           
+                                                                                                label='# of Episodes' }, 
+                                                                                                
+                                                                                             @{ expression={
+                                                                                             
+                                                                                                                ("Television Show")
+                                                                                                                
+                                                                                                           }; 
+                                                                                                           
+                                                                                               label='Media Type' }) 
+
+$Rpt += Get-HTMLContentClose
+$Rpt += Get-HTMLClosePage
+Save-HTMLReport -ReportContent $rpt -ShowReport  $Rp
