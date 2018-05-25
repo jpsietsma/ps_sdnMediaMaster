8ad4d1209c0f2591adddff3d38cd19c12d636d43
Jimmy <jpsietsma@users.noreply.github.com>
2018-05-25 00:32:36 -0400

    Added Components
    Pie Charts representing free drive space added.  Tabs for different media searches also added, both features still in development.

--- generateMasterReport.ps1
+++ generateMasterReport.ps1
@@ -12,10 +12,48 @@ $config_reportImagePath = 'images\sdnReportLogo.jpg'
 $existingShows = Get-ChildItem $config_TVShowDrives -Directory | SELECT FullName, Name, Size | SORT name
 
 $Rpt = @()
-$Rpt += get-htmlopenpage -TitleText "Media Master report" -LeftLogoString $config_reportImagePath -RightLogoName Alternate
-$Rpt += Get-HTMLContentOpen
+$Rpt += get-htmlopenpage -TitleText "Television Library - Master Admin Report" -LeftLogoString $config_reportImagePath -RightLogoName Alternate
+$Rpt += Get-HTMLContentOpen -HeaderText "Television Show Storage Overview"
+	
+    $tvStorageDrives = @('E:', 'G:', 'H:', 'I:')
+    
+    $count = 0
 
-$Rpt += get-htmlcontentdatatable -ArrayOfObjects ($existingShows | SELECT Name, FullName, @{ expression={
+    foreach ($drive in $tvStorageDrives){
+            
+            $count++
+
+            $driveSpace = Get-WMIObject Win32_Logicaldisk -filter "deviceid='$drive'" -ComputerName jimmybeast-sdn | SELECT DeviceID, @{Name='UsedSpace';Expression={($_.Size/1GB -as [int]) - ([math]::Round($_.Freespace/1GB,2))}},
+
+                                                                                                                            @{Name="freespace";Expression={[math]::Round($_.Freespace/1GB,2)}} | group DeviceID, freespace, UsedSpace
+
+            $Rpt += Get-HTMLColumnOpen -ColumnNumber $count -ColumnCount ($tvStorageDrives).count
+
+                $headerText = Get-WMIObject Win32_Logicaldisk -filter "deviceid='$drive'" -ComputerName jimmybeast-sdn | SELECT VolumeName 
+
+                $rpt += Get-HTMLHeading -headerSize 1 -headingText $headerText
+                
+                $PieObject = Get-HTMLPieChartObject
+
+                $rpt += Get-HTMLPieChart -ChartObject $PieObject -DataSet $driveSpace
+
+	        $Rpt += Get-HTMLColumnClose
+	        
+        }
+
+$Rpt += Get-HTMLContentclose
+
+	
+$tabarray = @('All Television','Active ONLY Shows','Movies','Statistics')
+$Rpt += Get-HTMLTabHeader -TabNames $tabarray 
+
+foreach ($tab in $tabarray ){
+
+	if($tab -eq 'All Television'){
+        
+        $rpt += get-htmltabcontentopen -TabName $tab
+        $Rpt += Get-HTMLContentOpen
+        $Rpt += get-htmlcontentdatatable -ArrayOfObjects ($existingShows | SELECT Name, FullName, @{ expression={
 
                                                                                                             $name = $_.FullName
                                                                                                             $name = $name -split("\\")
@@ -60,7 +98,20 @@ $Rpt += get-htmlcontentdatatable -ArrayOfObjects ($existingShows | SELECT Name, 
                                                                                                            }; 
                                                                                                            
                                                                                                label='Media Type' }) 
+        $rpt += get-htmltabcontentClose
+        $Rpt += Get-HTMLContentClose
 
-$Rpt += Get-HTMLContentClose
+
+    } elseif($tab -eq 'Active ONLY Shows'){
+
+    } elseif($tab -eq 'Movies'){
+
+    } elseif($tab -eq 'Statistics'){
+
+    }
+
+}
+
+
 $Rpt += Get-HTMLClosePage
 Save-HTMLReport -ReportContent $rpt -ReportPath 'D:\XAMPP\htdocs\media\docs\' -ReportName "masterTV" -showreport
