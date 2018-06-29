7a63d8c3ae1199635c086c65c4375e31a125f72b
Jimmy S <jpsietsma@gmail.com>
2018-06-29 09:54:54 -0400

    Added SDN Rss Feed
    Major code reorganization for RSS Feeds tab.  Added SDN RSS personal feed and removed YIFY Feed as it was notoriously unreliable.

--- generateMasterReport.ps1
+++ generateMasterReport.ps1
@@ -24,15 +24,16 @@ $existingShows = Get-ChildItem $config_TVShowDrives -Directory | SELECT FullName
 $existingMovies = Get-Childitem $config_MoviesDrives -Recurse -File | SELECT Name, FullName, Size | SORT name
 
 $rpt = @()
-$rpt += get-htmlopenpage -TitleText "SDN Media Libraries - Master Admin Report" -LeftLogoString $config_reportImagePath -RightLogoName Alternate
+$rpt += get-htmlopenpage -TitleText "SDN Media Libraries - Master Admin Report" -LeftLogoString $config_reportImagePath
 	
-$tabarray = @('Overview','Active ONLY Shows','Movies','All Television', 'Sort Drive', 'Missing Episodes', 'Media Health', 'RSS Feeds')
+$tabarray = @('Overview','Active Shows','Movies','All Television', 'Sort Drive', 'Missing Episodes', 'Media Health', 'RSS Feeds')
 $rpt += Get-HTMLTabHeader -TabNames $tabarray 
 
 foreach ($tab in $tabarray ){
 
 	if($tab -eq 'All Television'){
-        
+
+#region Television Tab
         $rpt += get-htmltabcontentopen -TabName $tab -TabHeading $tab
         $rpt += Get-HTMLContentOpen
         $rpt += get-htmlcontentdatatable -ArrayOfObjects ($existingShows | SELECT Name, FullName, @{ expression={
@@ -46,6 +47,7 @@ foreach ($tab in $tabarray ){
                                                                                                                 "G:" { $airStatus = 'Ended' }
                                                                                                                 "H:" { $airStatus = 'Active' }
                                                                                                                 "I:" { $airStatus = 'Ended' }
+                                                                                                                "Q:" { $airStatus = 'Ended' }
 
                                                                                                                 default { $airStatus = 'unknown' }
 
@@ -83,9 +85,11 @@ foreach ($tab in $tabarray ){
         $rpt += get-htmltabcontentClose
         $rpt += Get-HTMLContentClose
 
+#endregion
 
     } elseif($tab -eq 'Overview'){
 
+#region Overview Tab
         $rpt += get-htmltabcontentopen -TabName $tab -TabHeading $tab
         $rpt += Get-HTMLContentOpen -HeaderText "SDN Media Storage Overview"
     
@@ -99,7 +103,7 @@ foreach ($tab in $tabarray ){
 
                 $headerText = Get-WMIObject Win32_Logicaldisk -filter "deviceid='$drive`:'" -ComputerName jimmybeast-sdn | SELECT VolumeName 
 
-                $rpt += Get-HTMLHeading -headerSize 1 -headingText $headerText.VolumeName
+                $rpt += Get-HTMLHeading -headingText $headerText.VolumeName
                 
                 $dataset = @()
 
@@ -122,8 +126,8 @@ foreach ($tab in $tabarray ){
 
                 $PieObject = Get-HTMLPieChartObject
                 $PieObject.ChartStyle.ColorSchemeName = "ColorScheme4"
-                $pieObject.Size.Width = '250'
-                $pieObject.Size.Height = '250'
+                $pieObject.Size.Width = '225'
+                $pieObject.Size.Height = '225'
 
                 $rpt += Get-HTMLPieChart -ChartObject $PieObject -DataSet $dataset
 
@@ -134,62 +138,70 @@ foreach ($tab in $tabarray ){
         $rpt += Get-HTMLContentClose
 
         $rpt += Get-HTMLContentOpen -HeaderText "Bi-Monthly Download History Breakdown"
-        
-        #Gathering TV data and building chart
-        $tvDropDir = "s:\~drops\tvdrop"
-        $tvFilter = '*.sdn_tv_added'
 
+#region TV Torrent Bar Chart
         $tvChart = Get-HTMLBarChartObject
         $tvChart.Title = "TV Torrent Downloads - Last 14 Days"
+
+        $tvDropDir = "s:\~drops\tvdrop"
+        $tvFilter = '*.sdn_tv_added'
         $tvDataSet = SDN-BuildChartDataSet -DataDirectory $tvDropDir -FileFilter $tvFilter
-
-        #Gathering Software/Game data and building chart
-        $softwareDropDir = "s:\~drops\softwaredrop"
-        $softwareFilter = '*.sdn_software_added'
-
-        $softwareChart = Get-HTMLBarChartObject
-        $softwareChart.Title = "Software/Game Torrent Downloads - Last 14 Days"
-        $softwareDataSet = SDN-BuildChartDataSet -DataDirectory $softwareDropDir -FileFilter $softwareFilter
-
-        #Gathering Movie data and building chart
-        $movieDropDir = "s:\~drops\moviedrop"
-        $movieFilter = '*.sdn_movie_added'
-
-        $movieChart = Get-HTMLBarChartObject
-        $movieChart.Title = "Movie Torrent Downloads - Last 14 Days"
-        $movieDataSet = SDN-BuildChartDataSet -DataDirectory $movieDropDir -FileFilter $movieFilter
-
 
         $rpt += Get-HTMLColumnOpen -ColumnNumber 1 -ColumnCount 3
         $rpt += Get-HTMLContentOpen -HeaderText "TV Torrent Downloads - Last 14 Days" -IsHidden
         $rpt += Get-HTMLBarChart -ChartObject $tvChart -DataSet $tvDataSet 
         $rpt += Get-HTMLContentClose
         $rpt += Get-HTMLColumnClose
+#endregion
+
+#region Movie Torrent Bar Chart
+        $movieChart = Get-HTMLBarChartObject
+        $movieChart.Title = "Movie Torrent Downloads - Last 14 Days"
+        
+        $movieDropDir = "s:\~drops\moviedrop"
+        $movieFilter = '*.sdn_movie_added'
+        $movieDataSet = SDN-BuildChartDataSet -DataDirectory $movieDropDir -FileFilter $movieFilter
+
         $rpt += Get-HTMLColumnOpen -ColumnNumber 2 -ColumnCount 3
         $rpt += Get-HTMLContentOpen -HeaderText "Movie Torrent Downloads - Last 14 Days" -IsHidden
         $rpt += Get-HTMLBarChart -ChartObject $movieChart -DataSet $movieDataSet 
         $rpt += Get-HTMLContentClose
         $rpt += Get-HTMLColumnClose
+#endregion
+
+#region Software/Game Bar Chart
+        $softwareChart = Get-HTMLBarChartObject
+        $softwareChart.Title = "Software/Game Torrent Downloads - Last 14 Days"
+
+        $softwareDropDir = "s:\~drops\softwaredrop"
+        $softwareFilter = '*.sdn_software_added'
+        $softwareDataSet = SDN-BuildChartDataSet -DataDirectory $softwareDropDir -FileFilter $softwareFilter
+
         $rpt += Get-HTMLColumnOpen -ColumnNumber 3 -ColumnCount 3
         $rpt += Get-HTMLContentOpen -HeaderText "Game/Software Torrent Downloads - Last 14 Days" -IsHidden
         $rpt += Get-HTMLBarChart -ChartObject $softwareChart -DataSet $softwareDataSet 
         $rpt += Get-HTMLContentClose
         $rpt += Get-HTMLColumnClose
+#endregion
 
         $rpt += Get-HtmlContentClose
+
         $rpt += get-htmltabcontentClose
+#endregion
 
-    } elseif($tab -eq 'Active ONLY Shows'){
+    } elseif($tab -eq 'Active Shows'){ 
 
+#region Active Shows Tab
         $rpt += get-htmltabcontentopen -TabName $tab -TabHeading $tab
         $rpt += Get-HTMLContentOpen -HeaderText "Coming Soon..." -IsHidden
         $rpt += Get-HTMLContentText -Heading "Active TV Show Search" -Detail "This feature is currently under development, please check back soon. "
         $rpt += Get-HtmlContentClose
         $rpt += get-htmltabcontentClose 
+#endregion
 
     } elseif($tab -eq 'Movies'){
 
-        
+#region Movies Tab       
         $rpt += get-htmltabcontentopen -TabName $tab -TabHeading $tab
         $rpt += Get-HTMLContentOpen
         $rpt += get-htmlcontentdatatable -ArrayOfObjects ($existingMovies | SELECT Name, FullName, @{ expression={
@@ -203,6 +215,7 @@ foreach ($tab in $tabarray ){
                                                                                                                 "G:" { $airStatus = 'Ended' }
                                                                                                                 "H:" { $airStatus = 'Active' }
                                                                                                                 "I:" { $airStatus = 'Ended' }
+                                                                                                                "Q:" { $airStatus = 'Ended' }
 
                                                                                                                 default { $airStatus = 'unknown' }
 
@@ -231,11 +244,11 @@ foreach ($tab in $tabarray ){
                                                                                                label='Media Type' }) 
         $rpt += get-htmltabcontentClose
         $rpt += Get-HTMLContentClose
+#endregion       
 
-        
+    } elseif($tab -eq 'Sort Drive'){  
 
-    } elseif($tab -eq 'Sort Drive'){
-
+#region Sort Drive Tab too
         $rpt += get-htmltabcontentopen -TabName $tab -TabHeading $tab
          
          $fileList = Get-ChildItem 'S:' -File | SELECT Name, FullName, CreationTime
@@ -245,36 +258,58 @@ foreach ($tab in $tabarray ){
 
         $rpt += get-htmltabcontentClose 
         $rpt += Get-HtmlContentClose 
+#endregion
 
     } elseif($tab -eq 'Missing Episodes'){
 
+#region Missing Episodes Tab
         $rpt += get-htmltabcontentopen -TabName $tab -TabHeading $tab
         $rpt += Get-HTMLContentOpen -HeaderText "Coming Soon..." -IsHidden
         $rpt += Get-HTMLContentText -Heading "Missing Episode Report" -Detail "This feature will show all missing episodes on currently downloaded shows.  This feature is currently under development, please check back soon. "
         $rpt += Get-HtmlContentClose
         $rpt += get-htmltabcontentClose 
+#endregion
 
     } elseif($tab -eq 'Media Health'){
 
+#region Media Health Tab
         $rpt += get-htmltabcontentopen -TabName $tab -TabHeading $tab
         $rpt += Get-HTMLContentOpen -HeaderText "Coming Soon..." -IsHidden
         $rpt += Get-HTMLContentText -Heading "Media Health Status Report" -Detail "This feature will report on shows where episode names are not properly formatted, as well as any file type violations.  This feature is currently under development, please check back soon. "
         $rpt += Get-HtmlContentClose
         $rpt += get-htmltabcontentClose 
+#endregion
 
     } elseif($tab -eq 'RSS Feeds'){
 
-        $eztvRSSResults = SDN-SearchRSS "https://eztv.ag/ezrss.xml"
-        $limeRSSResults = SDN-SearchRSS "https://www.limetorrents.cc/rss/"
-        $yifyRSSResults = SDN-SearchRSS "https://yts.am/rss"
-
+#region RSS Feeds Tab
         $rpt += get-htmltabcontentopen -TabName $tab -TabHeading $tab
+        
+#region Creates Personal RSS data table
+        $personalRSSResults = SDN-SearchRSS "http://showrss.info/user/179679.rss?magnets=true&namespaces=true&name=null&quality=null&re=null"
+        $personalItems = @()
 
+        foreach($result in $personalRSSResults){
+
+            $url = 'URL01' + $result.enclosure.url + 'URL02' + $title + 'URL03'
+            $title = $result.title
+            $upDate = $result.pubDate
+            $description = $result.description
+
+            $personalItem = @([pscustomobject]@{title=$title;link=$url;uploadDate=$upDate;description=$description})
+            $personalItems += $personalItem
+
+        }
+
+        $rpt += Get-HTMLContentOpen -HeaderText "SDN Media RSS Feed"
+        $rpt += get-htmlcontentdatatable -ArrayOfObjects ($personalItems)
+        $rpt += Get-HTMLContentClose
+#endregion
+
+#region Creates Eztv RSS data table 
+        $eztvRSSResults = SDN-SearchRSS "https://eztv.ag/ezrss.xml"
         $eztvItems = @()
-        $limeItems = @()
-        $yifyItems = @()
 
-        #Creates EZTV RSS data table
         foreach($eztv in $eztvRSSResults){
 
             $url = 'URL01' + $eztv.enclosure.url + 'URL02' + $filename + 'URL03'
@@ -286,44 +321,35 @@ foreach ($tab in $tabarray ){
             $eztvItems += $eztvItem
 
         }
+        
+        $rpt += Get-HTMLContentOpen -HeaderText "EZTV.ag RSS Feed" -IsHidden
+        $rpt += get-htmlcontentdatatable -ArrayOfObjects ($eztvItems)
+        $rpt += Get-HTMLContentClose
+#endregion
 
-        #Creates YIFY RSS data table
-        foreach($yify in $yifyRSSResults){
+#region Creates LimeTorrents RSS data table
+        $limeRSSResults = SDN-SearchRSS "https://www.limetorrents.cc/rss/"
+        $limeItems = @()
 
-            $url = 'URL01' + $yify.enclosure.url + 'URL02' + $title + 'URL03'
-            $title = $yify.title
-            $upDate = $yify.pubDate
-            $description = $yify.description
-
-            $yifyItem = @([pscustomobject]@{title=$title;link=$url;uploadDate=$upDate;description=$description})
-            $yifyItems += $yifyItem
-
-        }
-
-        #Creates LimeTorrents RSS data table
         foreach($lime in $limeRSSResults){
         
             $url = 'URL01' + $lime.enclosure.url + 'URL02' + $title + 'URL03'
             $title = $lime.title
             $size = $lime.size
             
-            $limeItem = @([pscustomobject]@{title=$title;link=$url;'Size(MB)'=[math]::Round((($size/1024)/1024), 2)})
+            $limeItem = @([pscustomobject]@{title=$title;'Download URL'=$url;'Size (MB)'=[math]::Round((($size/1024)/1024), 2)})
             $limeItems += $limeItem
         
         }
-
-        $rpt += Get-HTMLContentOpen -HeaderText "EZTV.ag RSS Feed" -IsHidden
-        $rpt += get-htmlcontentdatatable -ArrayOfObjects ($eztvItems)
-        $rpt += Get-HTMLContentClose       
+               
         $rpt += Get-HTMLContentOpen -HeaderText "Limetorrents.cc RSS Feed" -IsHidden
         $rpt += get-htmlcontentdatatable -ArrayOfObjects ($limeItems)
         $rpt += Get-HTMLContentClose
-        $rpt += Get-HTMLContentOpen -HeaderText "Yts.am RSS Feed" -IsHidden
-        $rpt += get-htmlcontentdatatable -ArrayOfObjects ($yifyItems)
-        $rpt += Get-HTMLContentClose
-
+#endregion
 
         $rpt += get-htmltabcontentClose
+#endregion
+
     }
 }
 
